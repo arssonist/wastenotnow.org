f175c8afc86bc816ca48f32dc47ed6069c728ac0
Your Name <arssonist@yahoo.com>
2017-05-16 20:22:54 -0400

    initial; post form catching form in req.body

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var fs = require('fs');
+var nodemailer = require('nodemailer');
+var mg = require('nodemailer-mailgun-transport');
+var ncof = require('nconf');
+var auth = require('./config.json')
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// log requests to stdout and also
+// log HTTP requests to a file in combined format
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', { flags: 'a' });
+app.use(logger('dev'));
+app.use(logger('combined', { stream: accessLogStream }));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+var mg = require('nodemailer-mailgun-transport');
+var auth = require('../config.json')
+var nodemailerMailgun = nodemailer.createTransport(mg(auth));
+
+// router.use(logger('dev'));
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Waste Not Now' })
+});
+
+// 404 for page not found requests
+router.get(function (request, response) {
+  response.sendFile(path.join(__dirname + '/assets/404.html'));
+});
+
+// http GET /about
+router.get("/about", function (request, response) {
+  response.send('about page');
+ })
+
+// http GET /contact
+router.get("/contact", function (req, res) {
+  res.send('contact page')
+ })
+
+ // http POST /contact
+router.post("/", function (req, res) {
+    console.log('Username: ' + req.body.username)
+    console.log('Password: ' + req.body.email)
+    // var apikey = auth.api_key;
+    // var domain = auth.domain
+    // var mailgun = require('mailgun-js')({apiKey: apikey, domain: domain});
+    // //
+    // var data = {
+    //   from: 'Mail Gun <postmaster@sandbox7989cfe70dee4e4bbadb0439ff151517.mailgun.org>',
+    //   to: req.
+    // //   subject:
+    //   text: 'Testing some Mailgun awesomness!'
+    // };
+    //
+    // mailgun.messages().send(data, function (error, body) {
+    //   console.log(body);
+    // });
+});
+
+
+module.exports = router;
